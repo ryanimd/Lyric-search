71fc935a5217a1c6a05e3253e6a76f850ae3238d
Ryani <ryanid.@Yannis-MacBook-Air-3.local>
2021-04-12 14:59:50 -0400

    working to get lyrics on page

--- app.js
+++ app.js
@@ -2,34 +2,30 @@ $(() => { //Window Onload Begin
   $('.button').on('click', (event) => {
 //Prevent refresh on button click
     event.preventDefault()
+    const userArtist = $('#user-artist').val()
+    const userSong = $('#user-song').val()
+
+
 
 //Global Variables
-    const $userArtist = $('input[type="text"]').val()//Input should be song artist name
-    const $userSong = $('input[type="text"]').val()//Input should be song name
-    const $newSearch = $('input[type="submit"]').val()
-
-    const myKey = '&apikey=8054e1ba07f12b743c7ad8e5acfbbab9'
+    let artist = 'Prince'
+    let song = 'When Doves Cry'
 
     $.ajax({
-      url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=${$userSong}&q_artist=${$userArtist}&apikey=	8054e1ba07f12b743c7ad8e5acfbbab9`,
-      data: {}
-        // "$limit": $userArtist,}
+      url: `https://api.lyrics.ovh/v1/${artist}/${song}`
     }).then(
       (data) => {
         console.log(data);
 
-//         for(let i = 0; i < 15; i++){
-//           const $lyrics = $("<div>").addClass('lyrics').text(data.data[i].title)
-//           $('.search-results').append($lyrics)
-// }
+        // for (let i = 0; i < 15; i++){
+        //   const $lyrics = $('<div>').addClass('lyrics').text(data.lyrics)
+        //   $('.search-results').append($lyrics)
+        // }
+    // })
       },
       () => {
           console.log('bad request');
       }
     )
   })
-
 })//Window Onload End
-
-//////////////// Failed Attempts /////////////////////////
-// `https://api.musixmatch.com/ws/1.1/`
