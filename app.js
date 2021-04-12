55553d5af5364ade13ff68731ce822a106500995
Ryani <ryanid.@Yannis-MacBook-Air-3.local>
2021-04-11 23:11:53 -0400

    found api that works

--- app.js
+++ app.js
@@ -6,20 +6,22 @@ $(() => { //Window Onload Begin
 //Global Variables
     const $userArtist = $('input[type="text"]').val()//Input should be song artist name
     const $userSong = $('input[type="text"]').val()//Input should be song name
-    //
+    const $newSearch = $('input[type="submit"]').val()
+
+    const myKey = '&apikey=8054e1ba07f12b743c7ad8e5acfbbab9'
 
     $.ajax({
-      url: `https://api.lyrics.ovh/suggest/${$userArtist}`,
-      data: {
-        "$limit": $userArtist,}
+      url: `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=${$userSong}&q_artist=${$userArtist}&apikey=	8054e1ba07f12b743c7ad8e5acfbbab9`,
+      data: {}
+        // "$limit": $userArtist,}
     }).then(
       (data) => {
         console.log(data);
 
-        for(let i = 0; i < 15; i++){
-          const $lyrics = $("<div>").addClass('lyrics').text(data.data[i].title)
-          $('.search-results').append($lyrics)
-}
+//         for(let i = 0; i < 15; i++){
+//           const $lyrics = $("<div>").addClass('lyrics').text(data.data[i].title)
+//           $('.search-results').append($lyrics)
+// }
       },
       () => {
           console.log('bad request');
@@ -30,3 +32,4 @@ $(() => { //Window Onload Begin
 })//Window Onload End
 
 //////////////// Failed Attempts /////////////////////////
+// `https://api.musixmatch.com/ws/1.1/`
