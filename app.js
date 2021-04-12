ff0df0314edb8828cfdc183c6fba2b240cb87c49
Ryani <ryanid.@Yannis-MacBook-Air-3.local>
2021-04-12 17:54:21 -0400

    created h1 to display artist and song title

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(() => { //Window Onload Begin
   $('form').on('submit', (event) => {
-//Prevent refresh on button click
+    //Prevent refresh on button click
     event.preventDefault()
+    //Clears the page for a new search
     $('.search-results').empty()
 
 //Global Variables
@@ -15,13 +16,12 @@ $(() => { //Window Onload Begin
       url: `https://api.lyrics.ovh/v1/${artist}/${song}`
     }).then(
       (data) => {
-        console.log(data);
-
         for (let i = 0; i < 1; i++){
           const $lyrics = $('<div>').addClass('lyrics').text(data.lyrics)
           $('.search-results').append($lyrics)
-          // const $h1 = $('<h1>')
-          // $('.search-results').append($h1).text(userArtist)
+          const $h1 = $('<h1>')
+          $h1.text(`${userArtist} - ${userSong}`)
+          $('.lyrics').prepend($h1)
         }
     // })
       },
